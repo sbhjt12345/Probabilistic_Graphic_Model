0289fd3ecf92157c534343502595e878ac4acf7c
Jiantao Hu <jh2468@cornell.edu>
2017-03-23 00:29:33 +0900

    Week3 should be reviewed

--- HW3/ComputeSingletonFactors.m
+++ HW3/ComputeSingletonFactors.m
@@ -25,4 +25,11 @@ factors = repmat(struct('var', [], 'card', [], 'val', []), n, 1);
 
 % Your code here:
 
+for i=1:n,
+	factors(i).val = [i];
+	factors(i).card = imageModel.K;
+	factors(i).val = ComputeImageFactor(images(i).img,imageModel);
+end
+
+
 end
