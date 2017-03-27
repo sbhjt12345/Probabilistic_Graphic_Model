2923056b7fde77b203cfe431c682aa4f94262acd
Jiantao Hu <jh2468@cornell.edu>
2017-03-28 01:29:06 +0900

    did sth

--- HW3/ComputeSingletonFactors.m
+++ HW3/ComputeSingletonFactors.m
@@ -25,8 +25,8 @@ factors = repmat(struct('var', [], 'card', [], 'val', []), n, 1);
 
 % Your code here:
 
-for i=1:n,
-	factors(i).val = [i];
+for i=1:n
+	factors(i).var = [i];
 	factors(i).card = imageModel.K;
 	factors(i).val = ComputeImageFactor(images(i).img,imageModel);
 end
